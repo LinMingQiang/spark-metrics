876aee62150e23fe085d1570b5484cb27d0aa725
LinMingQiang <1356469429@qq.com>
2019-09-22 10:01:36 +0800

    spark + grafana + prometheus

---
+++ README.md
@@ -0,0 +1,9 @@
+Grafana + Prometheus
+
+1： 安装 Grafana <br>
+2： 安装 Prometheus <br>
+3： 安装 pushgateway （用于java client 自定义发送） <br>
+4： 安装 graphite_exporter <br>
+（用于spark自带的graphite metrics，见配置metrics.properties。可看到spark运行时的jvm内存等状态）<br>
+
+
---
+++ src/main/scala/com/spark/metrics/PushGatewayTest.scala
@@ -0,0 +1,34 @@
+package com.spark.metrics
+
+import io.prometheus.client.{CollectorRegistry, Gauge}
+import io.prometheus.client.exporter.PushGateway
+
+/**
+  * @author ${user.name}
+  */
+object PushGatewayTest {
+
+  def foo(x: Array[String]) = x.foldLeft("")((a, b) => a + b)
+
+  def main(args: Array[String]) {
+    val registry = new CollectorRegistry();
+
+    val pushGateway = new PushGateway("localhost:9091")
+    val g = Gauge
+      .build()
+      .name("test_name") // 查询专用
+      .help("注释")
+      .labelNames("topic", "partition") // 在展示折线图等的线
+      .register(registry)
+
+    while(true){
+      for (i <- 1 to 10) {
+         g.labels("test_topics", i.toString) // lable的值，一个值对应一条线
+          .set(System.currentTimeMillis() % 100 * i)
+      }
+      pushGateway.push(registry, "lmq_job_name_pushgateway")
+      Thread.sleep(500)
+    }
+  }
+
+}
